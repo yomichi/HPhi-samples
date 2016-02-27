a941c2c71696a30d9b92e7e91a475ad882813b59
Yuichi Motoyama <yomichi@tsg.jp>
2016-02-27 17:52:24 +0900

    update

--- MagnetizationProcess.sh/mag.sh
+++ MagnetizationProcess.sh/mag.sh
@@ -2,20 +2,23 @@
 
 hphi="../HPhi"
 
-hs=`seq 0.0 0.25 10.0`
+#Ls="4 6 8 10"
+Ls="10"
+hs=`seq 0.0 0.1 10.0`
 
-output="mag.dat"
+for L in ${Ls[@]}; do
+  result="mag-L${L}.dat"
+  stdout="hphi-${L}.out"
+  stderr="hphi-${L}.err"
 
-rm -f $output
-
-rm -f hphi.out hphi.err
-
-for h in ${hs[@]}; do
-  cp StdFace.template StdFace.def
-  echo "h = $h" >> StdFace.def
-  $hphi -s StdFace.def >> hphi.out 2>> hphi.err
-  L=`awk '$1=="L" {print $3}' StdFace.def`
-  mag=$( echo $(awk '$1=="Sz" {print -$2}' output/zvo_energy.dat) / $L | bc -l)
-  echo "$h $mag" | tee -a $output
+  rm -f $result $stdout $stderr
+  for h in ${hs[@]}; do
+    cp StdFace.template StdFace.def
+    echo "L = $L" >> StdFace.def
+    echo "h = $h" >> StdFace.def
+    $hphi -s StdFace.def >> $stdout 2>> $stderr
+    L=`awk '$1=="L" {print $3}' StdFace.def`
+    mag=`gawk --assign L=$L '$1=="Sz" {print -$2 / L}' output/zvo_energy.dat`
+    echo "$h $mag" | tee -a $result
+  done
 done
-
