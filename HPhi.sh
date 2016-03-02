6f634898469dec9e03a224722517cbb658e8b9a0
Yuichi Motoyama <yomichi@tsg.jp>
2016-03-02 16:11:03 +0900

    fixed HPhi.sh

--- HPhi.sh
+++ HPhi.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ -e ./HPhi ]; then
-  ./HPhi $@
+scriptdir=$(cd $(dirname $0); pwd)
+
+if [ -e ${scriptdir}/HPhi ]; then
+  ${scriptdir}/HPhi $@
 else
   which HPhi > /dev/null 2>/dev/null
   if [ $? == 0 ]; then
     HPhi $@
   else
-    scriptdir=$(cd $(dirname $0); pwd)
     cat << EOF
 HPhi is not found.
 Make a symbolic link "${scriptdir}/HPhi" pointing to HPhi executable
